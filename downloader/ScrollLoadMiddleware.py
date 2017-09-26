f39a3a8805e1cc691ebf151438e5c5dc47efa76b
siguoya <924714558@qq.com>
2017-09-26 15:43:34 +0800

    scroll_timeout改为在request meta中定义

--- downloader/ScrollLoadMiddleware.py
+++ downloader/ScrollLoadMiddleware.py
@@ -4,16 +4,18 @@ import os
 
 
 # 针对下拉加载的内容进行自动化滚动爬取
-# spider中需定义browser，scroll_timeout
+# spider中需定义browser
+# meta中需要定义scroll_timeout
 class ScrollLoadMiddleware(object):
     def process_request(self, request, spider):
         scroll_load = request.meta.get('scroll_load', False)
         if scroll_load:
+            scroll_timeout = request.meta.get('scroll_timeout', 3)
             spider.browser.get(request.url)
             content = open(os.path.dirname(__file__) + '/js/scroll.js').read()
             spider.browser.execute_script(content)
             try:
-                wait = ui.WebDriverWait(spider.browser, spider.scroll_timeout)
+                wait = ui.WebDriverWait(spider.browser, scroll_timeout)
                 wait.until(
                     lambda browser: browser.find_element_by_tag_name('body').get_attribute('has_more') is not None)
             except Exception as e:
